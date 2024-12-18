9968f2f37321b88a2f8be09d8ac6d7c300c0e12a
FathZulfaAli <fathzulfa34@gmail.com>
2024-12-19 01:30:33 +0700

    fix: hero image cdn

--- public/cdns/heroImage.cdn.ts
+++ public/cdns/heroImage.cdn.ts
@@ -1,5 +1,5 @@
 export const flyingThingsUrl =
-  "https://res.cloudinary.com/dzdrs7dkj/image/upload/f_auto,q_auto/v1/fm-portfolio-assets/qf0wus2z3o7downd2s8export y";
+  "https://res.cloudinary.com/dzdrs7dkj/image/upload/f_auto,q_auto/v1/fm-portfolio-assets/qf0wus2z3o7downd2s8y";
 
 export const heroPhoto =
   "https://res.cloudinary.com/dzdrs7dkj/image/upload/f_auto,q_auto/v1/fm-portfolio-assets/iu0ed4ajj3cydjfdaczg";
