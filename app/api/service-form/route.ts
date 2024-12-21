844f9f80296b573e4078779fa113a6e5ea960ff5
FathZulfaAli <fathzulfa34@gmail.com>
2024-12-21 21:13:59 +0700

    refactor: migrate from nodemailer to Resend for email handling and update email data types

--- app/api/service-form/route.ts
+++ app/api/service-form/route.ts
@@ -1,43 +1,28 @@
 import { NextResponse } from "next/server";
-import { transporter } from "./nodemailer.config";
-import { ClientEmailData } from "@/src/types/nodemailer.types";
+import { ClientEmailData } from "@/src/types/collabForm.types";
+import { Resend } from "resend";
 import "dotenv/config";
-import path from "path";
-import { readFileSync } from "fs";
-import Handlebars from "handlebars";
-
-const myEmail = process.env.SMTP_USER?.toString();
-const VIPEmail = process.env.NOTIFICATION_EMAIL?.toString();
 
 export async function POST(request: Request) {
   try {
+    const privateKey = process.env.RESEND_KEY;
+    const notificationEmail = process.env.NOTIFICATION_EMAIL;
+
+    if (!privateKey || !notificationEmail) {
+      return NextResponse.json({
+        status: 500,
+        error: "Please contact me to fix the problem.",
+      });
+    }
+
     const body = await request.json();
-    console.log("Request body:", body);
-
     const { clientEmail, clientName, description }: ClientEmailData = body;
+    const resend = new Resend(privateKey);
 
-    // access email template
-    const templateMail = path.join(
-      process.cwd(),
-      "/src/emails",
-      "clientConfirmationEmail.hbs",
-    );
-    const templateSource = readFileSync(templateMail, "utf-8");
-    const complileTemplate = Handlebars.compile(templateSource);
-
-    // Send confirmation email to the client
-    await transporter.sendMail({
-      from: myEmail,
-      to: clientEmail,
-      subject: "We've Received Your Email!",
-      html: complileTemplate({ clientName: clientName }), // Use the generated HTML
-    });
-
-    // Send notification email to yourself
-    await transporter.sendMail({
-      from: myEmail,
-      to: VIPEmail,
-      subject: "New Service Form Submission",
+    resend.emails.send({
+      from: "Fath Tech Web <onboarding@resend.dev>",
+      to: notificationEmail,
+      subject: `New Submission Received from ${clientName}`,
       text: `A new submission has been received:\n\nName: ${clientName}\nEmail: ${clientEmail}\nDescription: ${description}`,
     });
 
@@ -46,7 +31,6 @@ export async function POST(request: Request) {
       message: "Emails sent successfully!",
     });
   } catch (error) {
-    console.error("Error sending emails:", error);
     return NextResponse.json({
       status: 500,
       error: error || "An error occurred while sending emails.",
